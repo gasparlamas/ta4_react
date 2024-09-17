d2679998a87794c493203206b5c977798b98e6ff
gasparlamas <gasparlamas32@gmail.com>
2024-09-17 16:27:04 -0300

    ta4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+const [valor,setValor] = useState(0);
+
+const sumar = () =>{
+  setValor(valor+1);
+}
+
+
+const restar = () =>{
+  if (valor>0){setValor(valor-1);}
+  
+}
+
+const reiniciar = () =>{
+  setValor(0);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contador</h1>
+      <h2>{valor}</h2>
+      <button onClick={sumar}>Aumentar</button>
+      <button onClick={restar}>Decrementar</button>
+      <button onClick={reiniciar}>Reiniciar</button>
     </div>
   );
 }
